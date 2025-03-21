8fed8bd0c3332f7aeb00be622ca33729b535db37
Sergey Dubovyk <knidarkness@gmail.com>
2025-03-21 07:21:55 +0100

    feat: do shifting more "in-place" with no explicit ring data structures

--- src/main/java/com/knidarkness/MatrixRotator.java
+++ src/main/java/com/knidarkness/MatrixRotator.java
@@ -1,93 +1,73 @@
 package com.knidarkness;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class MatrixRotator {
 
     public int[][] rotateMatrix(int[][] matrix, int r) {
-        List<List<Integer>> rings = getMatrixRings(matrix);
-        for (List<Integer> ring : rings) {
-            Collections.rotate(ring, -(r % ring.size()));
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int[][] rotated = new int[height][width];
+
+        int numRings = Math.min(height, width) / 2;
+        for (int ring = 0; ring < numRings; ring++) {
+            processRing(matrix, rotated, ring, r);
         }
-        return assembleMatrix(rings, matrix[0].length, matrix.length);
+        return rotated;
     }
 
-    public List<Integer> getMatrixRing(int[][] matrix, int ring) {
-        List<Integer> result = new ArrayList<>();
-        int matrixHeight = matrix.length;
-        int matrixWidth = matrix[0].length;
+    public void processRing(int[][] matrix, int[][] rotated, int ring, int shift) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int ringWidth = width - 2 * ring;
+        int ringHeight = height - 2 * ring;
 
-        // Top row
-        for (int j = ring; j < matrixWidth - ring; j++) {
-            result.add(matrix[ring][j]);
+        int ringLength = 2 * ringWidth + 2 * (ringHeight - 2);
+        int effectiveShift = shift % ringLength;
+
+        for (int index = 0; index < ringLength; index++) {
+            int newIndex = (index - effectiveShift + ringLength) % ringLength;
+
+            int[] src = getCoordsForRingPosition(width, height, ring, index);
+            int[] dst = getCoordsForRingPosition(width, height, ring, newIndex);
+
+            rotated[dst[0]][dst[1]] = matrix[src[0]][src[1]];
         }
-        // Right column
-        for (int i = ring + 1; i < matrixHeight - 1 - ring; i++) {
-            result.add(matrix[i][matrixWidth - 1 - ring]);
-        }
-        // Bottom row, reversed!
-        for (int j = matrixWidth - 1 - ring; j >= ring; j--) {
-            result.add(matrix[matrixHeight - 1 - ring][j]);
-        }
-        // Left column
-        for (int i = matrixHeight - 2 - ring; i > ring; i--) {
-            result.add(matrix[i][ring]);
-        }
-        return result;
     }
 
-    public List<List<Integer>> getMatrixRings(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int numRings = Math.min(rows, cols) / 2;
-        List<List<Integer>> rings = new ArrayList<>();
-        for (int i = 0; i < numRings; i++) {
-            rings.add(getMatrixRing(matrix, i));
-        }
-        return rings;
-    }
+    public int[] getCoordsForRingPosition(int fullWidth, int fullHeight, int ring, int index) {
+        int ringWidth = fullWidth - 2 * ring;
+        int ringHeight = fullHeight - 2 * ring;
 
-    public int[][] assembleMatrix(List<List<Integer>> rings, int width, int height) {
-        int[][] matrix = new int[height][width];
-        for (int ring = 0; ring < rings.size(); ring++) {
-            assembleRing(matrix, rings.get(ring), ring);
-        }
-        return matrix;
-    }
+        int columnElements = ringHeight - 2;  // column elements without top/bottom row
+        int rightColumnX = fullWidth - 1 - ring;
+        int bottomRowY = fullHeight - 1 - ring;
 
-    public void assembleRing(int[][] matrix, List<Integer> ring, int i) {
-        int matrixWidth = matrix[0].length;
-        int matrixHeight = matrix.length;
-
-        int ringWidth = matrixWidth - 2 * i;
-        int ringHeight = matrixHeight - 2 * i;
-
-        // Top row
-        for (int j = 0; j < ringWidth; j++) {
-            matrix[i][i + j] = ring.get(j);
-        }
-        // Right column
-        for (int j = 1; j < ringHeight - 1; j++) {
-            matrix[i + j][matrixWidth - 1 - i] = ring.get((ringWidth - 1) + j);
-        }
-        // Bottom row
-        int bottomRowStart = ringWidth + ringHeight - 2;
-        for (int j = 0; j < ringWidth; j++) {
-            matrix[matrixHeight - 1 - i][i + j] = ring.get(bottomRowStart + (ringWidth - 1 - j));
-        }
-        for (int j = 1; j < ringHeight - 1; j++) {
-            int leftColStart = (ringWidth + ringHeight - 2) + ringWidth - 1;
-            matrix[matrixHeight - 1 - i - j][i] = ring.get(leftColStart + j);
+        if (index < ringWidth) {
+            // Top row: no need to shift, can already return
+            return new int[]{ring, ring + index};
+        } else if (index < ringWidth + columnElements) {
+            // Right column: "skip" the top row
+            int localIndex = index - ringWidth;
+            return new int[]{ring + localIndex + 1, rightColumnX};
+        } else if (index < ringWidth + columnElements + ringWidth) {
+            // Bottom row: skip top row & right column, then go left from the right
+            int localIndex = index - (ringWidth + columnElements);
+            return new int[]{ bottomRowY, rightColumnX - localIndex };
+        } else {
+            // Left column: Skip prev. rows & columns
+            // then same as for bottom row - with index increasing -> go bottom to top
+            int localIndex = index - (ringWidth + columnElements + ringWidth);
+            return new int[]{ bottomRowY - localIndex - 1, ring };
         }
     }
 
     public void logMatrix(int[][] matrix) {
         for (int[] row : matrix) {
-            String rowStr = Arrays.stream(row).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+            String rowStr = Arrays.stream(row)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "));
             System.out.println(rowStr);
         }
     }
--- src/test/java/com/knidarkness/MatrixRotatorTest.java
+++ src/test/java/com/knidarkness/MatrixRotatorTest.java
@@ -2,9 +2,6 @@ package com.knidarkness;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
 
 class MatrixRotatorTest {
 
@@ -72,154 +69,50 @@ class MatrixRotatorTest {
     }
 
     @Test
-    void testGetOuterLine() {
-        int[][] matrix = {
-            {1, 2},
-            {3, 4}
-        };
-
+    void testGetCoordsForRingPositionOuter4x4() {
         MatrixRotator matrixRotator = new MatrixRotator();
-        List<Integer> result = matrixRotator.getMatrixRing(matrix, 0);
-        List<Integer> expected = Arrays.asList(1, 2, 4, 3);
-        assertEquals(expected, result);
+        int[][] expectedCoords = {
+                {0, 0},
+                {0, 1},
+                {0, 2},
+                {0, 3},
+                {1, 3},
+                {2, 3},
+                {3, 3},
+                {3, 2},
+                {3, 1},
+                {3, 0},
+                {2, 0},
+                {1, 0}
+        };
+        for (int i = 0; i < expectedCoords.length; i++) {
+            int[] coords = matrixRotator.getCoordsForRingPosition(
+                    4,
+                    4,
+                    0,
+                    i);
+            assertArrayEquals(expectedCoords[i], coords, "Mismatch at index " + i);
+        }
     }
 
     @Test
-    void testGetInnerLineSmall() {
-        int[][] matrix = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
+    void testGetCoordsForRingPositionInner4x6() {
+        MatrixRotator matrixRotator = new MatrixRotator();
+
+        // ring 1 for a 4x6 matrix (see testRotateMatrix4x6)
+        int[][] expectedCoords = {
+                {1, 1},
+                {1, 2},
+                {2, 2},
+                {3, 2},
+                {4, 2},
+                {4, 1},
+                {3, 1},
+                {2, 1},
         };
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        List<Integer> result = matrixRotator.getMatrixRing(matrix, 1);
-        List<Integer> expected = Arrays.asList(6, 7, 11, 10);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void testGetOuterLine6x4() {
-        int[][] matrix = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16},
-            {17, 18, 19, 20},
-            {21, 22, 23, 24}
-        };
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        List<Integer> result = matrixRotator.getMatrixRing(matrix, 0);
-        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 8, 12, 16, 20, 24, 23, 22, 21, 17, 13, 9, 5);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void testGetInnerLine() {
-        int[][] matrix = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16},
-            {17, 18, 19, 20},
-            {21, 22, 23, 24}
-        };
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        List<Integer> result = matrixRotator.getMatrixRing(matrix, 1);
-        List<Integer> expected = Arrays.asList(6, 7, 11, 15, 19, 18, 14, 10);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void testGetInnerLine2() {
-        int[][] matrix = {
-            {1, 2, 3, 4, 5, 6},
-            {7, 8, 9, 10, 11, 12},
-            {13, 14, 15, 16, 17, 18},
-            {19, 20, 21, 22, 23, 24}
-        };
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        List<Integer> result = matrixRotator.getMatrixRing(matrix, 1);
-        List<Integer> expected = Arrays.asList(8, 9, 10, 11, 17, 16, 15, 14);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void testGetMatrixLines() {
-        int[][] matrix = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16},
-            {17, 18, 19, 20},
-            {21, 22, 23, 24}
-        };
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        List<List<Integer>> rings = matrixRotator.getMatrixRings(matrix);
-        List<List<Integer>> expectedRings = Arrays.asList(
-            Arrays.asList(1, 2, 3, 4, 8, 12, 16, 20, 24, 23, 22, 21, 17, 13, 9, 5),
-            Arrays.asList(6, 7, 11, 15, 19, 18, 14, 10)
-        );
-        assertEquals(expectedRings, rings);
-
-        int[][] reassembled = matrixRotator.assembleMatrix(rings, 4, 6);
-        assertMatrixEquals(matrix, reassembled);
-    }
-
-    @Test
-    void testAssembleMatrixEmpty() {
-        List<List<Integer>> rings = new ArrayList<>();
-        MatrixRotator matrixRotator = new MatrixRotator();
-        int[][] result = matrixRotator.assembleMatrix(rings, 0, 0);
-        int[][] expected = new int[0][0];
-        assertMatrixEquals(expected, result);
-    }
-
-    @Test
-    void testAssembleMatrix2x2Zeros() {
-        List<List<Integer>> rings = List.of(Arrays.asList(0, 0, 0, 0));
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        int[][] result = matrixRotator.assembleMatrix(rings, 2, 2);
-        int[][] expected = {
-            {0, 0},
-            {0, 0}
-        };
-        assertMatrixEquals(expected, result);
-    }
-
-    @Test
-    void testAssembleMatrix2x2_1234() {
-        List<List<Integer>> rings = List.of(Arrays.asList(1, 2, 4, 3));
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        int[][] result = matrixRotator.assembleMatrix(rings, 2, 2);
-        int[][] expected = {
-            {1, 2},
-            {3, 4}
-        };
-        assertMatrixEquals(expected, result);
-    }
-
-    @Test
-    void testAssembleMatrix2x4_12345678() {
-        List<List<Integer>> rings = List.of(
-                Arrays.asList(1, 2, 4, 6, 8, 7, 5, 3)
-        );
-
-        MatrixRotator matrixRotator = new MatrixRotator();
-        int[][] result = matrixRotator.assembleMatrix(rings, 2, 4);
-        int[][] expected = {
-            {1, 2},
-            {3, 4},
-            {5, 6},
-            {7, 8}
-        };
-        assertMatrixEquals(expected, result);
+        for (int i = 0; i < expectedCoords.length; i++) {
+            int[] coords = matrixRotator.getCoordsForRingPosition(4, 6, 1, i);
+            assertArrayEquals(expectedCoords[i], coords, "Mismatch at index " + i);
+        }
     }
 }
